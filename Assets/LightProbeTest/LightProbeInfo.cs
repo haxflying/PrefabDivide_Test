4c0b371fde903bdc1040f1d9254900d04b4b689c
赵一鸣 <912778422@qq.com>
2018-03-13 15:11:11 +0800

    Added lightprobe data displacement

--- Assets/LightProbeTest/LightProbeInfo.cs
+++ Assets/LightProbeTest/LightProbeInfo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using Sirenix.OdinInspector;
 using UnityEditor;
+using SimpleLightProbePlacer;
 public class LightProbeInfo : MonoBehaviour {
 
     SphericalHarmonicsL2[] shs;
@@ -14,18 +15,94 @@ public class LightProbeInfo : MonoBehaviour {
         shs = LightmapSettings.lightProbes.bakedProbes;
         poss = LightmapSettings.lightProbes.positions;
         print(shs.Length + " " + poss.Length);
+
+        SHStrorage[] storages = GameObject.FindObjectsOfType<SHStrorage>();
+        for (int i = 0; i < poss.Length; i++)
+        {
+            foreach (var s in storages)
+            {
+                LightProbeVolume vol = s.GetComponentInChildren<LightProbeVolume>();
+                if(vol != null)
+                {
+                    if(vol.GetBounds().Contains(poss[i]))
+                    {
+                        s.sh_Indexs.Add(i);
+                        s.sh_datas.Add(shs[i]);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     [Button(ButtonSizes.Medium)]
     void Swap()
     {
-        for (int i = 0; i < 8; i++)
+        SHStrorage[] storages = GameObject.FindObjectsOfType<SHStrorage>();
+        Vector3 temp = storages[0].transform.position;
+        storages[0].transform.position = storages[1].transform.position;
+        storages[1].transform.position = temp;
+        if (storages[0].sh_Indexs.Count != storages[1].sh_Indexs.Count)
         {
-            var temp = shs[i];
-            shs[i] = shs[i + 8];
-            shs[i + 8] = temp;
+            Debug.LogError("sh count not equal");
+            return;
         }
-        LightmapSettings.lightProbes.bakedProbes = shs;
+
+        int stepLength = storages[0].sh_datas.Count;
+        List<SphericalHarmonicsL2> new_shs = new List<SphericalHarmonicsL2>(shs.Length);
+        foreach (var s in storages)
+        {
+            s.Inserted = false;
+        }
+
+        for (int i = 0; i < poss.Length; i++)
+        {
+            foreach (var s in storages)
+            {
+                if (s.Inserted)
+                    continue;
+
+                LightProbeVolume vol = s.GetComponentInChildren<LightProbeVolume>();
+                if (vol != null)
+                {
+                    if (vol.GetBounds().Contains(poss[i]))
+                    {
+                        int offset = Mathf.FloorToInt((float)i / (float)s.sh_Indexs.Count);
+                        new_shs.InsertRange(offset * stepLength, s.sh_datas);
+                        s.Inserted = true;
+                    }
+                }
+            }
+        }
+
+            //if (storages[0].sh_Indexs.Count == storages[1].sh_Indexs.Count)
+            //{
+            //    for (int i = 0; i < storages[0].sh_Indexs.Count; i++)
+            //    {
+            //        var temp = shs[storages[0].sh_Indexs[i]];
+            //        shs[storages[0].sh_Indexs[i]] = shs[storages[1].sh_Indexs[i]];
+            //        shs[storages[1].sh_Indexs[i]] = temp;
+            //    }
+            //}
+            //else
+            //{
+            //    Debug.LogError("sh count not equal");
+            //}
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    var temp = shs[i];
+            //    shs[i] = shs[i + 8];
+            //    shs[i + 8] = temp;
+            //}
+            LightmapSettings.lightProbes.bakedProbes = new_shs.ToArray();
+    }
+
+    [Button(ButtonSizes.Medium)]
+    void ClearStorage()
+    {
+        SHStrorage[] storages = GameObject.FindObjectsOfType<SHStrorage>();
+        foreach (var s in storages)
+            s.sh_Indexs.Clear();
     }
 
     private void OnDrawGizmos()
---
+++ Assets/LightProbeTest/SHStrorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+public class SHStrorage : MonoBehaviour {
+    public bool Inserted;
+    /// <summary>
+    /// 并没有什么卵用
+    /// </summary>
+    public List<int> sh_Indexs = new List<int>();
+    public List<SphericalHarmonicsL2> sh_datas = new List<SphericalHarmonicsL2>();
+}
